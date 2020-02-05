bbae89115bdb49ede636d58f8ee1da00a64a95a9
marethyun <bacci.ange@gmail.com>
2020-02-05 12:20:36 +0100

    Worked on events and cards

---
+++ server/src/main/java/com/iutdeck/server/GameContext.java
@@ -0,0 +1,7 @@
+package com.iutdeck.server;
+
+public class CastContext {
+    // TODO Implementer
+
+    // TODO Doit permettre d'accéder aux éléments du jeu (GameObjects), aux gestionnaires d'évènements,
+}
